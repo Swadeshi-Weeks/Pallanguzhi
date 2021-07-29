1b45c65c066a14673ff4805a20bf9abaef592c20
Vedanth Ramji <86137377+Vedanth-Ramji@users.noreply.github.com>
2021-07-29 19:25:57 +0530

    reinitialized repo, we can work smoothly now

---
+++ components/coin.js
@@ -0,0 +1,11 @@
+function Coin(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.initialCount = 5;
+
+    this.render = function () {
+
+    }
+}
