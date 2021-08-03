ff2bfbe9f73331209f401890ba8bd8ab0c3f9aa5
Pranav Kethireddy <pran.kethi@gmail.com>
2021-08-02 18:47:33 -0700

    made code cleaner

--- sketch.js
+++ sketch.js
@@ -1,39 +1,41 @@
 function setup() {
     // game setup
-    createCanvas(4000, 4000);
+    createCanvas(1500, 1500);
     
     // color setup
     let bc = color(234,172,145);
     let hc = color(166,147,127);
     
+    // variable setup
+    let hx = 560
+    let hy = 300
+    let hd = 75
+    let hi = 0
+
+    let hx1 = 560
+    let hy1 = 390
+    let hd1 = 75
+    let hi1 = 0
+
     // board setup
     fill(bc);
     noStroke();
     rect(500, 100, 700, 300);
-    fill(hc);
-    noStroke();
-    ellipse(830, 170, 500, 130)
-    circle(560, 300, 75);
-    circle(650, 300, 75);
-    circle(740, 300, 75);
-    circle(830, 300, 75);
-    circle(920, 300, 75);
-    circle(1010, 300, 75);
-    circle(1100, 300, 75);
-    line(500, 250, 1100, 300)
-    fill(bc);
-    noStroke();
     rect(500, 350, 700, 250);
     fill(hc);
     noStroke();
+    ellipse(830, 170, 500, 130)
     ellipse(830, 520, 500, 130)
-    circle(560, 390, 75);
-    circle(650, 390, 75);
-    circle(740, 390, 75);
-    circle(830, 390, 75);
-    circle(920, 390, 75);
-    circle(1010, 390, 75);
-    circle(1100, 390, 75);
+    while(hi <= 6) {
+        circle(hx, hy, hd)
+        hx = hx + 90
+        hi = hi + 1 
+    }
+    while(hi1 <= 6) {
+        circle(hx1, hy1, hd1)
+        hx1 = hx1 + 90
+        hi1 = hi1 + 1 
+    }
 }
 
 function draw() {
